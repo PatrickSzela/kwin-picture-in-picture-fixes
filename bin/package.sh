ae506a55b8e520e18e3cbf9e14255764094d4dfb
Patryk Szela <53914832+PatrickSzela@users.noreply.github.com>
2025-10-07 01:53:48 +0200

    fix: archive's root being named dist instead of project's ID

--- bin/package.sh
+++ bin/package.sh
@@ -2,8 +2,10 @@
 set -e
 
 NAME=$(jq -r '.name' ./package.json)
+ID="$(echo "$NAME" | sed -E 's/-([a-z])/\U\1/g')"
 VERSION=$(jq -r '.version' ./package.json)
 
 mkdir -p builds
 
 7z a -tzip "builds/${NAME}-${VERSION}.kwinscript" ./dist
+7z rn "./builds/${NAME}-${VERSION}.kwinscript" dist "${ID}"
